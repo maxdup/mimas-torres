0343b708ecb807944bbc54da181e622b1bcbb32d
maxdup <mdupuis@hotmail.ca>
2017-02-09 23:36:24 -0500

    fixed route problems

--- torres/main.py
+++ torres/main.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__, template_folder='templates')
-folio = Blueprint('folio', __name__, template_folder='templates')
+folio = Blueprint('folio', __name__, template_folder='templates', static_folder='static')
 
 @main.route('/admin/')
 def index():
     return render_template('index.html')
 
-@folio.route('/')
-def index():
-    return render_template('portfolio.html')
+@folio.route('/', defaults={'path': '', 'param': ''})
+@folio.route('/<path:path>', defaults={'param': ''})
+@folio.route('/<path:path>/<path:param>')
+def index(path, param):
+    return folio.send_static_file('partials/main.html')
